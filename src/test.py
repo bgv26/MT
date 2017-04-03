035a9c5258abe79d40af644c0a15a7fa1bb03a67
bgv26 <bgv26@mail.ru>
2017-04-03 11:43:33 +0300

    Add support for multiple directories. Some small improvements.

--- src/test.py
+++ src/test.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from enum import Enum
-
+# from enum import Enum
+import os
 from lxml import etree
 
 # definitions
+DIRECTORIES = (
+    r'D:\Devel\MT\src',
+)
+
+IN_FILE = 'bncat.xml'
+
+OUT_FILE = 'cian_new.xml'
+
 ID_PREFIX = "1508"
 
 BLOCK_PHRASES = (
@@ -111,154 +119,154 @@ PROPORTIONS = (
 )
 
 
-class AdType(Enum):
-    RENT = 'flats_rent'
-    SALE = 'flats_for_sale'
-    COMMERCE = 'commerce'
-    SUBURBIAN = 'suburbian'
-
-
-class ForDay(Enum):
-    UNLIM = 0
-    FOR_DAY = 1
-    FOR_MONTH = 2
-
-
-class CommerceType(Enum):
-    OFFICE = 'O'
-    WAREHOUSE = 'W'
-    TRADE_PLACE = 'T'
-    FOR_FOOD_PLACE = 'F'
-    FREE_PLACE = 'FP'
-    GARAGE = 'G'
-    AUTOSERVICE = 'AU'
-    BUILDING = 'B'
-    LEGAL_ADDRESS = 'UA'
-    SALE_OF_BUSINESS = 'SB'
-    DOMESTIC_SERVICES = 'BU'
-
-
-class ContractType(Enum):
-    RENT = 1
-    SUBRENT = 2
-    RENT_RIGHT_SALE = 3
-    OBJECT_SALE = 4
-    SHARE_BUSINESS = 5
-
-
-class BuildingType(Enum):
-    NONLIVING = 1
-    LIVING = 2
-
-
-class BuildingEnter(Enum):
-    FREE = 1
-    PERMISSION = 2
-
-
-class BuildingStatus(Enum):
-    SATISFACTORY = 1
-    GOOD = 2
-    PERFECT = 3
-
-
-class BuildingClass(Enum):
-    A = 1
-    APLUS = 2
-    B = 3
-    BPLUS = 4
-    C = 5
-    CPLUS = 6
-
-
-class HouseType(Enum):
-    PANEL = 1
-    BRICK = 2
-    MONOLITH = 3
-    BRICK_MONOLITH = 4
-    BLOCK = 5
-    WOOD = 6
-    STALIN = 7
-    OLD_FOND = 9
-
-
-class SaleTypeFirst(Enum):
-    FREE = 'free'
-    ALTERNATE = 'alt'
-    INVEST = 'invest'
-    PDKP = 'pdkp'
-    PEREUSTUPKA = 'pereustupka'
-    ZHSK = 'zhsk'
-    DDU = 'ddu'
-
-
-class RoomNum(Enum):
-    ROOM = 0
-    SINGLE_ROOM = 1
-    DOUBLE_ROOM = 2
-    TRIPLE_ROOM = 3
-    FORTH_ROOM = 4
-    FIVE_ROOM = 5
-    MULTI_ROOM = 6
-    FREE = 7
-    STUDIO = 9
-    BED = 10
-
-
-class Windows(Enum):
-    YARD = 1
-    STREET = 2
-    MIX = 3
-
-
-class Ipoteka(Enum):
-    YES = 1
-    NO = 0
-
-
-class Repair(Enum):
-    COSMETIC = 1
-    EURO = 2
-    DESIGN = 3
-    ABSENT = 4
-
-
-class Finishing(Enum):
-    YES = 1
-    NO = 0
-
-
-class SaleTypeUsed(Enum):
-    FREE = 'F'
-    ALTERNATE = 'A'
-
-
-class Deposit(Enum):
-    YES = 0
-    NO = 1
-
-
-class Composition(Enum):
-    ANY = 1
-    FAMILY = 2
-    FEMALE = 3
-    MALE = 4
-
-
-class RealityType(Enum):
-    HOUSE = 'K'
-    PART_OF_HOUSE = 'P'
-    STEAD = 'A'
-    TOWNHOUSE = 'T'
-
-
-class LandType(Enum):
-    SNT = 1
-    IGS = 2
-    PROM = 3
-    FARM = 4
-    DNP = 5
-    INVEST = 6
+# class AdType(Enum):
+#     RENT = 'flats_rent'
+#     SALE = 'flats_for_sale'
+#     COMMERCE = 'commerce'
+#     SUBURBIAN = 'suburbian'
+#
+#
+# class ForDay(Enum):
+#     UNLIM = 0
+#     FOR_DAY = 1
+#     FOR_MONTH = 2
+#
+#
+# class CommerceType(Enum):
+#     OFFICE = 'O'
+#     WAREHOUSE = 'W'
+#     TRADE_PLACE = 'T'
+#     FOR_FOOD_PLACE = 'F'
+#     FREE_PLACE = 'FP'
+#     GARAGE = 'G'
+#     AUTOSERVICE = 'AU'
+#     BUILDING = 'B'
+#     LEGAL_ADDRESS = 'UA'
+#     SALE_OF_BUSINESS = 'SB'
+#     DOMESTIC_SERVICES = 'BU'
+#
+#
+# class ContractType(Enum):
+#     RENT = 1
+#     SUBRENT = 2
+#     RENT_RIGHT_SALE = 3
+#     OBJECT_SALE = 4
+#     SHARE_BUSINESS = 5
+#
+#
+# class BuildingType(Enum):
+#     NONLIVING = 1
+#     LIVING = 2
+#
+#
+# class BuildingEnter(Enum):
+#     FREE = 1
+#     PERMISSION = 2
+#
+#
+# class BuildingStatus(Enum):
+#     SATISFACTORY = 1
+#     GOOD = 2
+#     PERFECT = 3
+#
+#
+# class BuildingClass(Enum):
+#     A = 1
+#     APLUS = 2
+#     B = 3
+#     BPLUS = 4
+#     C = 5
+#     CPLUS = 6
+#
+#
+# class HouseType(Enum):
+#     PANEL = 1
+#     BRICK = 2
+#     MONOLITH = 3
+#     BRICK_MONOLITH = 4
+#     BLOCK = 5
+#     WOOD = 6
+#     STALIN = 7
+#     OLD_FOND = 9
+#
+#
+# class SaleTypeFirst(Enum):
+#     FREE = 'free'
+#     ALTERNATE = 'alt'
+#     INVEST = 'invest'
+#     PDKP = 'pdkp'
+#     PEREUSTUPKA = 'pereustupka'
+#     ZHSK = 'zhsk'
+#     DDU = 'ddu'
+#
+#
+# class RoomNum(Enum):
+#     ROOM = 0
+#     SINGLE_ROOM = 1
+#     DOUBLE_ROOM = 2
+#     TRIPLE_ROOM = 3
+#     FORTH_ROOM = 4
+#     FIVE_ROOM = 5
+#     MULTI_ROOM = 6
+#     FREE = 7
+#     STUDIO = 9
+#     BED = 10
+#
+#
+# class Windows(Enum):
+#     YARD = 1
+#     STREET = 2
+#     MIX = 3
+#
+#
+# class Ipoteka(Enum):
+#     YES = 1
+#     NO = 0
+#
+#
+# class Repair(Enum):
+#     COSMETIC = 1
+#     EURO = 2
+#     DESIGN = 3
+#     ABSENT = 4
+#
+#
+# class Finishing(Enum):
+#     YES = 1
+#     NO = 0
+#
+#
+# class SaleTypeUsed(Enum):
+#     FREE = 'F'
+#     ALTERNATE = 'A'
+#
+#
+# class Deposit(Enum):
+#     YES = 0
+#     NO = 1
+#
+#
+# class Composition(Enum):
+#     ANY = 1
+#     FAMILY = 2
+#     FEMALE = 3
+#     MALE = 4
+#
+#
+# class RealityType(Enum):
+#     HOUSE = 'K'
+#     PART_OF_HOUSE = 'P'
+#     STEAD = 'A'
+#     TOWNHOUSE = 'T'
+#
+#
+# class LandType(Enum):
+#     SNT = 1
+#     IGS = 2
+#     PROM = 3
+#     FARM = 4
+#     DNP = 5
+#     INVEST = 6
 
 
 class BlockedRecordException(Exception):
@@ -272,62 +280,6 @@ class EmptyRequiredFieldException(Exception):
 class EmptyResult(Exception):
     pass
 
-
-# def to_cian(ad_type):
-#     root = etree.Element(ad_type)
-#     offer = etree.SubElement(root, 'offer')
-#     ad_id = etree.SubElement(offer, 'id')
-#
-#     if ad_type == 'commerce':  # for commerce only
-#         commerce_type = etree.SubElement(offer, 'commerce_type')
-#         contract_type = etree.SubElement(offer, 'contract_type')
-#         building = etree.SubElement(offer, 'building')
-#         building_dict = building.attrib
-#         building_dict['floor'] = ''
-#         building_dict['floor_total'] = ''
-#         business_shopping_center = etree.SubElement(offer, 'business_shopping_center')
-#
-#     if ad_type.startswith('flats'):  # for rent and sale
-#         rooms_num = etree.SubElement(offer, 'rooms_num')
-#         floor = etree.SubElement(offer, 'floor')
-#         floor_dict = floor.attrib
-#         floor_dict['total'] = ''
-#         composition = etree.SubElement(offer, 'composition')
-#
-#     if ad_type == 'suburbian':  # for suburbian only
-#         deal_type = etree.SubElement(offer, 'deal_type')
-#         realty_type = etree.SubElement(offer, 'realty_type')
-#         land_type = etree.SubElement(offer, 'land_type')
-#         floor_total = etree.SubElement(offer, 'floor_total')
-#         bedroom_total = etree.SubElement(offer, 'bedroom_total')
-#
-#     area = etree.SubElement(offer, 'area')
-#     area_dict = area.attrib
-#     if ad_type == 'suburbian':
-#         area_dict['region'] = ''
-#         area_dict['living'] = ''
-#     else:
-#         area_dict['total'] = ''
-#
-#     price = etree.SubElement(offer, 'price')
-#     phone = etree.SubElement(offer, 'phone')
-#     address = etree.SubElement(offer, 'address')
-#     address_dict = address.attrib
-#     address_dict['admin_area'] = str(39)
-#     address_dict['locality'] = ''
-#     address_dict['street'] = ''
-#
-#     note = etree.SubElement(offer, 'note')
-#     options = etree.SubElement(offer, 'options')
-#     com = etree.SubElement(offer, 'com')
-#     photo = etree.SubElement(offer, 'photo')
-#     promotions = etree.SubElement(offer, 'promotions')
-#     special_offer_id = etree.SubElement(offer, 'special_offer_id')
-#     if ad_type == 'flats_for_sale':  # for sale only
-#         residential_complex = etree.SubElement(offer, 'residential_complex')
-#         project_declaration = etree.SubElement(offer, 'project_declaration')
-#     return root
-#
 
 def gen_new_id(offer_id):
     return ID_PREFIX + offer_id[4:]
@@ -355,9 +307,9 @@ def is_block(text):
 
 def get_rooms_areas(rooms, area):
     # split living area to room's areas in fixed PROPORTIONS
-    areas = [round(area * room / 100, 1) for room in PROPORTIONS[rooms - 1][:-1]]
-    areas.append(round(area - sum(areas), 1))
-    return ' '.join(map(str, areas))
+    areas = [int(area * room / 100) for room in PROPORTIONS[rooms - 1][:-1]]
+    areas.append(area - sum(areas))
+    return ' '.join((str(int(x)) if x - int(x) == 0 else '{:.1f}'.format(x) for x in areas))
 
 
 def get_city_by_place(text):
@@ -378,7 +330,7 @@ def get_lot_number(offer_id):
 
 def get_ipoteka(item):
     ad_terms = get_node_value(item, 'additional-terms')
-    return 1 if ad_terms == 'Ипотека' else 0
+    return '1' if ad_terms == 'Ипотека' else '0'
 
 
 def from_bn(item):
@@ -429,11 +381,11 @@ def from_bn_flats_and_rooms(item, sell=True):
         if object_type == 'квартира':
             if sell:
                 info['options_ipoteka'] = get_ipoteka(item)
-            rooms_num = int(get_node_value(item, 'rooms-total', True))
-            info['rooms_num'] = str(rooms_num)
+            rooms_num = get_node_value(item, 'rooms-total', True)
+            info['rooms_num'] = rooms_num
         else:
-            rooms_num = int(get_node_value(item, 'rooms-offer', True))
-            info['rooms_num'] = str(0)
+            rooms_num = get_node_value(item, 'rooms-offer', True)
+            info['rooms_num'] = '0'
 
         info['floor_total'] = get_node_value(item, 'floors', True)
         info['floor'] = get_node_value(item, 'floor', True)
@@ -442,10 +394,10 @@ def from_bn_flats_and_rooms(item, sell=True):
         info['area_living'] = area_living
         info['area_kitchen'] = get_node_value(item, 'kitchen/value')
         info['area_total'] = get_node_value(item, 'total/value', True)
-        info['options_object_type'] = int(get_node_value(item, 'new-building')) + 1
+        info['options_object_type'] = str(int(get_node_value(item, 'new-building')) + 1)
 
         if area_living:
-            info['area_rooms'] = get_rooms_areas(rooms_num, float(area_living))
+            info['area_rooms'] = get_rooms_areas(int(rooms_num), float(area_living))
         else:
             print('Fix this (area living is empty) for advert id: [{}]'.format(ad_id))
 
@@ -499,9 +451,9 @@ def to_cian_flats_and_rooms(root_node, data):
     phone = etree.SubElement(offer, 'phone')
     phone.text = data['phone']
     etree.SubElement(offer, 'address', area='39', locality=data['address_locality'], street=data['address_street'])
-    options = etree.SubElement(offer, 'options', object_type=str(data['options_object_type']))
+    options = etree.SubElement(offer, 'options', object_type=data['options_object_type'])
     if 'options_ipoteka' in data:
-        options.set('ipoteka', str(data['options_ipoteka']))
+        options.set('ipoteka', data['options_ipoteka'])
     note = etree.SubElement(offer, 'note')
     note.text = etree.CDATA(data['note'])
     for p in data['photo']:
@@ -539,46 +491,48 @@ def to_cian_suburbians(root_node, data):
     note.text = etree.CDATA(data['note'])
 
 
-doc = etree.parse('bncat.xml')
-objects = doc.xpath('bn-object')
-f = open('cian_new.xml', 'w+', encoding='utf-8')
-flats_rooms_sales = [o for o in objects
-                     if o.xpath('type[text() = "квартира" or text() = "комната"] and action[text() = "продажа"]')]
-if len(flats_rooms_sales):
-    root = etree.Element('flats_for_sale')
-    for sale in flats_rooms_sales:
-        try:
-            to_cian_flats_and_rooms(root, from_bn_flats_and_rooms(sale))
-        except EmptyResult:
-            pass
-    f.write(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8'))
-flats_rooms_rents = [o for o in objects
-                     if o.xpath('type[text() = "квартира" or text() = "комната"] and action[text()="аренда"]')]
-if len(flats_rooms_rents):
-    root = etree.Element('flats_rent')
-    for sale in flats_rooms_rents:
-        try:
-            to_cian_flats_and_rooms(root, from_bn_flats_and_rooms(sale))
-        except EmptyResult:
-            pass
-    if len(flats_rooms_sales):
-        f.write(etree.tostring(root, pretty_print=True, encoding='utf-8').decode('utf-8'))
-    else:
-        f.write(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8'))
-suburbians = [o for o in objects
-              if o.xpath('type[text() = "дом" or text() = "коттедж" or text() = "участок"]')]
-if len(suburbians):
-    root = etree.Element('suburbian')
-    for sale in suburbians:
-        try:
-            to_cian_suburbians(root, from_bn_suburbians(sale))
-        except EmptyResult:
-            pass
-    if len(flats_rooms_sales) or len(flats_rooms_rents):
-        f.write(etree.tostring(root, pretty_print=True, encoding='utf-8').decode('utf-8'))
-    else:
-        f.write(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8'))
-# commerce = [o for o in objects
-#             if o.xpath('type[not(text() = "квартира" or text() = "комната" or '
-#                        'text() = "коттедж" or contains(text(), "дом"))]')]
-f.close()
+for cat in DIRECTORIES:
+    doc = etree.parse(os.path.join(cat, IN_FILE))
+    with open(os.path.join(cat, OUT_FILE), 'w+', encoding='utf-8') as f:
+        objects = doc.xpath('bn-object')
+        flats_rooms_sales = [o for o in objects
+                             if o.xpath('type[text() = "квартира" or text() = "комната"]'
+                                        ' and action[text() = "продажа"]')]
+        if len(flats_rooms_sales):
+            root = etree.Element('flats_for_sale')
+            for sale in flats_rooms_sales:
+                try:
+                    to_cian_flats_and_rooms(root, from_bn_flats_and_rooms(sale))
+                except EmptyResult:
+                    pass
+            f.write(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8'))
+        flats_rooms_rents = [o for o in objects
+                             if o.xpath('type[text() = "квартира" or text() = "комната"]'
+                                        ' and action[text()="аренда"]')]
+        if len(flats_rooms_rents):
+            root = etree.Element('flats_rent')
+            for sale in flats_rooms_rents:
+                try:
+                    to_cian_flats_and_rooms(root, from_bn_flats_and_rooms(sale))
+                except EmptyResult:
+                    pass
+            if len(flats_rooms_sales):
+                f.write(etree.tostring(root, pretty_print=True, encoding='utf-8').decode('utf-8'))
+            else:
+                f.write(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8'))
+        suburbians = [o for o in objects
+                      if o.xpath('type[text() = "дом" or text() = "коттедж" or text() = "участок"]')]
+        if len(suburbians):
+            root = etree.Element('suburbian')
+            for sale in suburbians:
+                try:
+                    to_cian_suburbians(root, from_bn_suburbians(sale))
+                except EmptyResult:
+                    pass
+            if len(flats_rooms_sales) or len(flats_rooms_rents):
+                f.write(etree.tostring(root, pretty_print=True, encoding='utf-8').decode('utf-8'))
+            else:
+                f.write(etree.tostring(root, pretty_print=True, xml_declaration=True, encoding='utf-8').decode('utf-8'))
+                # commerce = [o for o in objects
+                #             if o.xpath('type[not(text() = "квартира" or text() = "комната" or '
+                #                        'text() = "коттедж" or contains(text(), "дом"))]')]
