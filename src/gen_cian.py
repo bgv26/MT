d26436f05694e3a2f6c1edf79ced49252172ab5b
bgv26 <bgv26@mail.ru>
2017-04-06 12:19:38 +0300

    Create separate config file

--- src/gen_cian.py
+++ src/gen_cian.py
@@ -3,73 +3,7 @@
 import os
 from datetime import datetime as dt
 from lxml import etree
-
-# definitions
-HOME_DIR_MT = r'/export/cian'
-HOME_DIR_NIRLAN = r'/export/n_cian'
-
-DIRECTORIES = (
-    # r'C:\Devel\PyCharmProject\MT\src',
-    r'D:\Devel\MT\src',
-    # os.path.join(HOME_DIR_MT, 'tem'),
-    # os.path.join(HOME_DIR_MT, 'soc'),
-    # os.path.join(HOME_DIR_MT, 'bud'),
-    # os.path.join(HOME_DIR_MT, 'sev'),
-    # os.path.join(HOME_DIR_MT, 'zap'),
-    # os.path.join(HOME_DIR_MT, 'vos'),
-    # os.path.join(HOME_DIR_MT, 'str'),
-    # os.path.join(HOME_DIR_MT, 'bat'),
-    # os.path.join(HOME_DIR_NIRLAN, 'centr'),
-    # os.path.join(HOME_DIR_NIRLAN, 'sever'),
-    # os.path.join(HOME_DIR_NIRLAN, 'zapad'),
-)
-
-IN_FILE = 'bncat.xml'
-IN_FILE_COMMERCE = 'bncat_comnedv.xml'
-
-OUT_FILE = 'cian_new.xml'
-
-LOG_FILE = '{}.log'.format(dt.today().strftime('%d %B %Y %H-%M-%S'))
-
-ID_PREFIX = "1508"
-
-BLOCK_PHRASES = (
-    'ена 1',
-    'ена 2',
-    'ена: 1',
-    'ена: 2',
-    'найти',
-    'Продажа квартир',
-    'омощь',
-    'оможем',
-    'омогу',
-    'выбрать квартиру',
-    'выбор квартир',
-    'квартир мало',
-    'родажа',
-    'квартир ',
-    'варианты',
-    '1-комнатные',
-    '2х-комнатные',
-    '1-2-3 комнатные',
-    '1-2 комнатные',
-    'Есть варианты по этажам',
-    'квартиры на других этажах',
-    'выбор квартир',
-    'торговые площади',
-)
-
-OFFICES = {
-    '8(863)2-270-500': {'office': 'ДОНМТ офис Центральный 1', 'phone': '9381361278'},
-    '8(863)2-990-707': {'office': 'ДОНМТ офис Центральный 2', 'phone': '9604434798'},
-    '8(863)2-270-909': {'office': 'ДОНМТ офис Центральный 3', 'phone': '9286216057'},
-    '8(863)200-67-67': {'office': 'ДОНМТ офис Западный', 'phone': '9298174480'},
-    '8(863)2-300-909': {'office': 'ДОНМТ офис Северный', 'phone': '9885669794'},
-    '8(863)200-85-85': {'office': 'ДОНМТ офис Стройгородок', 'phone': '9034067095'},
-    '8(863)300-24-00': {'office': 'ДОНМТ офис Восточный', 'phone': '9045033362'},
-    '8(863)2-500-400': {'office': 'ДОНМТ офис Батайск', 'phone': '9281879795'},
-    '8(863)250-27-27': {'office': 'ДОНМТ офис Коммерческая недвижимость', 'phone': '9081719005'},
-}
+from config import DIRECTORIES, IN_FILE, IN_FILE_COMMERCE, LOG_FILE, OUT_FILE, ID_PREFIX, BLOCK_PHRASES, OFFICES
 
 
 class BlockedRecordException(Exception):
